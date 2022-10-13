1344f78c4887cad8bc3e0e8013d5cbb5d9392b10
nwahidm <nwahidm@gmail.com>
2022-10-13 18:05:02 +0700

    db

--- index.js
+++ index.js
@@ -1,1 +1,147 @@
 // database dan filter here
+let obj = [
+    {
+        eventType: 'Outdoor',
+        eventName: 'No Way Back',
+        month: 'January',
+        city: 'Jakarta',
+        price: 300000,
+        image: "/outdoor/JAN.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Second Piece of 12',
+        month: 'February',
+        city: 'Bali',
+        price: 150000,
+        image: "/outdoor/FEB.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Never Ending Fantasy',
+        month: 'March',
+        city: 'Lombok',
+        price: 250000,
+        image: "/outdoor/MAR.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'In Lyric We Loved',
+        month: 'April',
+        city: 'Bandung',
+        price: 150000,
+        image: "/outdoor/APR.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Together We Loud',
+        month: 'May',
+        city: 'Surabaya',
+        price: 150000,
+        image: "/outdoor/MAY.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Screaming Notes',
+        month: 'June',
+        city: 'Yogyakarta',
+        price: 100000,
+        image: "/outdoor/JUNE.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Hollow Voice',
+        month: 'July',
+        city: 'Malang',
+        price: 150000,
+        image: "/outdoor/JULY.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'RAP MON STER',
+        month: 'August',
+        city: 'Padang',
+        price: 200000,
+        image: "/outdoor/AUG.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Melody Magician',
+        month: 'September',
+        city: 'Bekasi',
+        price: 100000,
+        image: "/outdoor/SEP.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Music Realm',
+        month: 'October',
+        city: 'Palembang',
+        price: 150000,
+        image: "/outdoor/OCT.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'EDM Party',
+        month: 'November',
+        city: 'Jakarta',
+        price: 200000,
+        image: "/outdoor/NOV.PNG"
+    },
+    {
+        eventType: 'Outdoor',
+        eventName: 'Grand Slam',
+        month: 'December',
+        city: 'Bali',
+        price: 250000,
+        image: "/outdoor/DES.PNG"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: 'Major-ity',
+        month: 'Januari',
+        city: 'Lombok',
+        price: 200000,
+        image: "/indoor/major-ity-jan---15-www.event_music.com.png"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: 'Silent-ly',
+        month: 'Maret',
+        city: 'Bandung',
+        price: 250000,
+        image: "/indoor/silent-ly-mar---15-www.event_music.com.png"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: 'The Notes',
+        month: 'Mei',
+        city: 'Surabaya',
+        price: 200000,
+        image: "/indoor/the-notes-may--15-www.event_music.com.png"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: 'Hi-story',
+        month: 'Juli',
+        city: 'Yogyakarta',
+        price: 150000,
+        image: "/indoor/hi-story-july--15-www.event_music.com.png"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: 'Vio-lance',
+        month: 'September',
+        city: 'Malang',
+        price: 100000,
+        image: "/indoor/vio-lance-sept--15-www.event_music.com.png"
+    },
+    {
+        eventType: 'Indoor',
+        eventName: '___End',
+        month: 'November',
+        city: 'Padang',
+        price: 200000,
+        image: "/indoor/_-_-_--end-nov-.png"
+    }
+]
